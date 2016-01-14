c7fe4f65f078b0898555bc179f55515eec8122be
khsagehorn <huntersagehorn@gmail.com>
2016-01-14 14:20:07 -0700

    more tests

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log("sanity check!");
+
+var firstParagraph = document.getElementById('main');
+console.log(firstParagraph)
+
+var getAllElements = document.getElementsByTagName('ul');
+console.log(getAllElements)
+
+var getSingleElement = document.getElementsByTagName('ul')[0];
+console.log(getSingleElement)
+
+var allListItems = document.getElementsByTagName('li');
+console.log(allListItems)
+
+for (var i = 0; i < allListItems.length; i++) {
+  console.log(allListItems[i]);
+}
