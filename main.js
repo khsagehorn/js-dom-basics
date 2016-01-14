fbbe9763e6607447bc13e25739bcfa3a10b93b82
khsagehorn <huntersagehorn@gmail.com>
2016-01-14 14:55:38 -0700

    more js modifiers

--- main.js
+++ main.js
@@ -15,3 +15,55 @@ console.log(allListItems)
 for (var i = 0; i < allListItems.length; i++) {
   console.log(allListItems[i]);
 }
+
+var evenElements = document.getElementsByClassName('even');
+console.log(evenElements)
+
+var oddElements = document.getElementsByClassName('odd');
+console.log(oddElements)
+
+var firstEvenElement = document.getElementsByClassName('even')[0];
+console.log(firstEvenElement)
+
+var firstOddElement = document.getElementsByClassName('odd')[0];
+console.log(firstOddElement)
+
+var main = document.querySelector('#main');
+console.log(main)
+
+var firstOddElement = document.querySelector('.odd');
+console.log(firstOddElement)
+
+var oddAndEvenElements = document.querySelectorAll('.odd, .even');
+console.log(oddAndEvenElements);
+
+for (var i = 0; i < allListItems.length; i++) {
+  console.log(allListItems[i].innerText);
+}
+
+for (var i = 0; i < allListItems.length; i++) {
+  console.log(allListItems[i].innerText = i);
+}
+console.log(allListItems);
+
+document.getElementById('main').style.backgroundColor = "goldenrod";
+
+// document.getElementsByClassName.style.color = "red";
+
+// document.getElementsByClassName('li').style.color = "blue";
+
+var li = document.createElement('li');
+console.log(li);
+
+var newElement = li.innerText = "another list item";
+console.log(newElement);
+console.log(li);
+
+getSingleElement.appendChild(li);
+console.log(allListItems);
+
+var anotherListElement = document.createElement('li');
+anotherListElement.innerText = "prepend";
+console.log(anotherListElement);
+getSingleElement.insertBefore(anotherListElement, getSingleElement.firstChild);
+console.log(allListItems);
